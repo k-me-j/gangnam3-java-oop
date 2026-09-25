2f532594d515742c8ea64c15d934883021eb0eb0
kamie <k.me4dev@gmail.com>
2026-09-25 18:36:08 +0900

    feat: PoisonSlime: 독 공격으로 hp 감소하는 함수 추가

--- src/main/java/com/survivalcoding/day04_calss_instance/PoisonSlime.java
+++ src/main/java/com/survivalcoding/day04_calss_instance/PoisonSlime.java
@@ -1,5 +1,40 @@
 package com.survivalcoding.day04_calss_instance;
 
 public class PoisonSlime extends Slime {
-
+    private static final int POISON_DAMAGE_DIVISOR = 5;
+    private int poisonCount = 5;
+    
+    public PoisonSlime(String suffix) {
+        super(suffix);
+    }
+    
+    private int calculatePoisonDamage(int hp) {
+        if (hp <= 0) {
+            return 0;
+        }
+        
+        return hp / POISON_DAMAGE_DIVISOR;
+    }
+    
+    private void applyPoisonDamage(Hero hero) {
+        System.out.println("독 포자를 살포했다!");
+        
+        int damage = calculatePoisonDamage(hero.getHp());
+        hero.setHp(hero.getHp() - damage);
+        
+        poisonCount -= 1;
+        
+        System.out.printf("%d 포인트 데미지%n", damage);
+    }
+    
+    @Override
+    public void attack(Hero hero) {
+        super.attack(hero);
+        
+        if (poisonCount <= 0) {
+            return;
+        }
+        
+        applyPoisonDamage(hero);
+    }
 }
