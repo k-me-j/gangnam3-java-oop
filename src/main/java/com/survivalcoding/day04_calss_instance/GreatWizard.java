6a953e01040431ee0001d304cad2f14dc7280414
kamie <k.me4dev@gmail.com>
2026-09-25 21:45:12 +0900

    feat: GreatWizard: 생성자 추가

--- src/main/java/com/survivalcoding/day04_calss_instance/GreatWizard.java
+++ src/main/java/com/survivalcoding/day04_calss_instance/GreatWizard.java
@@ -1,5 +1,30 @@
 package com.survivalcoding.day04_calss_instance;
 
 public class GreatWizard extends Wizard {
-
+    static final int MAX_MP = 150;
+    
+    public GreatWizard() {
+        super("대마법", MAX_HP, MAX_MP);
+    }
+    
+    public GreatWizard(String name) {
+        super(name, MAX_HP, MAX_MP);
+    }
+    
+    public GreatWizard(String name, int hp) {
+        super(name, hp, MAX_MP);
+    }
+    
+    public GreatWizard(String name, int hp, int mp) {
+        super(name, hp, mp);
+    }
+    
+    public GreatWizard(String name, int hp, int mp, Wand wand) {
+        super(name, hp, mp, wand);
+    }
+    
+    @Override
+    public int getMaxMp() {
+        return MAX_MP;
+    }
 }
