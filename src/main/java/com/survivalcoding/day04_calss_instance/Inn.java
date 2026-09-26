91d808e0dc0379016789c380146b06455cd951a3
kamie <k.me4dev@gmail.com>
2026-09-26 09:59:31 +0900

    fix: Inn: 접근 지정자 추가

--- src/main/java/com/survivalcoding/day04_calss_instance/Inn.java
+++ src/main/java/com/survivalcoding/day04_calss_instance/Inn.java
@@ -3,7 +3,7 @@ package com.survivalcoding.day04_calss_instance;
 public class Inn {
     static final int INN_FEE_HP = 100;
     
-    void checkIn(Hero hero) {
+    public void checkIn(Hero hero) {
         int hero_hp = hero.getHp() - INN_FEE_HP;
         hero.setHp(hero_hp);
     }
